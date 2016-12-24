3a65660ebf55e936df8d5ec3f1bcee2372024955
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-24 17:11:52 +0100

    feat: network discovery
    initial set up for allowing client to automatically find server and join.

---
+++ gear-vr-leap-pc/Assets/gear-vr-leap/Scripts/OverridenNetworkDiscovery.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class OverridenNetworkDiscovery : NetworkDiscovery {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void OnReceivedBroadcast(string fromAddress, string data)
+	{
+		base.OnReceivedBroadcast(fromAddress, data);
+		NetworkManager.singleton.networkAddress = fromAddress;
+		NetworkManager.singleton.StartClient();
+	}
+}
